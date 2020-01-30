e52bfb62ffac9b09c89985fdfedecad1466fc152
Wgcsam <55789479+Wgcsam@users.noreply.github.com>
2020-01-30 17:17:51 +0000

    First Commit

---
+++ Source/Overflow/OverflowGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OverflowGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OVERFLOW_API AOverflowGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
